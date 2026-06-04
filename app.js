ea70a36eeb62b8eb13aaeeb1dbc8779615076af3
charleshenrigervasi88 <charleshenri.gervasi@gmail.com>
2026-06-04 13:58:27 +0200

    feat: add CI/CD pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function additionner(a, b) {
 return a + b;
 }
 function direBonjour(nom) {
-if (!nom) throw new Error("Un nom est requis !");
+if (!nom) throw new Error("Un nom est requis !!");
 return `Bonjour, ${nom} !`;
 }
 module.exports = { additionner, direBonjour };
